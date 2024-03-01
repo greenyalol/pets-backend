262b511edc4b9ada18bf8451bfa175a3efe683cc
Oleg Grinblat <greenblat.071@gmail.com>
2024-03-01 20:39:07 +0200

    added admin functionality and some hotfix

--- middlewares/protect-routes.js
+++ middlewares/protect-routes.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
+const { getUserRoles } = require('../services/db_services');
 
 function verifyUser(req, res, next) {
     const { token } = req.cookies; //middleware
     if (!token) {
-        res.send(403).json({ message: 'Access denied' });
+        res.status(403).json({ error: 'Access denied' });
     } else {
         try {
             const decoded = jwt.verify(token, process.env.PRIVATE_KEY);
@@ -11,12 +12,29 @@ function verifyUser(req, res, next) {
             //get user roleF
             next();
         } catch (err) {
-            res.send(403).json({ message: 'Access denied' });
+            //res.send(403).json({ error: 'Access denied' });
         }
     }
 }
 
+async function hasAdminRole(req, res, next) {
+    const { uid } = req;
+    try {
+        const userRoles = await getUserRoles(uid);
+        console.log(userRoles);
+        if (userRoles.some((user) => user.slug === 'admin')) {
+            next();
+        } else {
+            res.status(403).json({ error: 'Access denied' });
+            return;
+        }
+    } catch (err) {
+        // res.send(500).json({ error: 'Internal server error' });
+    }
+
+}
+
 module.exports = {
-    verifyUser
+    verifyUser, hasAdminRole
 };
 //verify user
