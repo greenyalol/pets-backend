b2f16cafe0f40217d3ef37000de125309b319eac
Oleg Grinblat <greenblat.071@gmail.com>
2024-03-02 13:33:12 +0200

    fix domain

--- endpoints.js
+++ endpoints.js
@@ -43,8 +43,8 @@ const app = express();
 
 app.use(cors(
     {
-        // origin: 'http://localhost:5173',
-        origin: true,
+        origin: 'http://pets-frontend-react.s3-website.eu-north-1.amazonaws.com',
+        // origin: true,
         credentials: true
     }));
 app.use(express.json());
